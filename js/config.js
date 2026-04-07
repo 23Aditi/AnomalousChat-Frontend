9176b0d21e09461f04db234f93dc4de70175374f
23Aditi <aditi23github@gmail.com>
2026-04-07 22:24:43 +0530

    updated frontend

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
     // Change this URL when deploying to production
-    BASE_URL: "http://13.217.211.79:5000"
+    BASE_URL: "https://defensive-suggest-vendors-restrict.trycloudflare.com"
 };
